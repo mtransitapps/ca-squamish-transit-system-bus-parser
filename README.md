aa49f198d2d3d388e042a08ad98f968db89ec53c
Mathieu Méa <mathieu.mea@gmail.com>
2015-05-27 18:57:21 -0400

    Initial Release.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# ca-squamish-transit-system-bus-parser
+ca-squamish-transit-system-bus-parser
+=====================================
+
 Parser for Squamish Transit System (BC Transit) Bus data
---
+++ src/org/mtransit/parser/ca_squamish_transit_system_bus/SquamishTransitSystemBusAgencyTools.java
@@ -0,0 +1,186 @@
+package org.mtransit.parser.ca_squamish_transit_system_bus;
+
+import java.util.HashSet;
+import java.util.regex.Pattern;
+
+import org.apache.commons.lang3.StringUtils;
+import org.mtransit.parser.DefaultAgencyTools;
+import org.mtransit.parser.Utils;
+import org.mtransit.parser.gtfs.data.GCalendar;
+import org.mtransit.parser.gtfs.data.GCalendarDate;
+import org.mtransit.parser.gtfs.data.GRoute;
+import org.mtransit.parser.gtfs.data.GSpec;
+import org.mtransit.parser.gtfs.data.GStop;
+import org.mtransit.parser.gtfs.data.GTrip;
+import org.mtransit.parser.mt.data.MAgency;
+import org.mtransit.parser.mt.data.MInboundType;
+import org.mtransit.parser.mt.data.MRoute;
+import org.mtransit.parser.mt.data.MSpec;
+import org.mtransit.parser.mt.data.MTrip;
+import org.mtransit.parser.mt.data.MTripStop;
+
+// http://bctransit.com/*/footer/open-data
+// http://bctransit.com/servlet/bctransit/data/GTFS.zip
+public class SquamishTransitSystemBusAgencyTools extends DefaultAgencyTools {
+
+	public static void main(String[] args) {
+		if (args == null || args.length == 0) {
+			args = new String[3];
+			args[0] = "input/gtfs.zip";
+			args[1] = "../../mtransitapps/ca-squamish-transit-system-bus-android/res/raw/";
+			args[2] = ""; // files-prefix
+		}
+		new SquamishTransitSystemBusAgencyTools().start(args);
+	}
+
+	private HashSet<String> serviceIds;
+
+	@Override
+	public void start(String[] args) {
+		System.out.printf("Generating Squamish Transit System bus data...\n");
+		long start = System.currentTimeMillis();
+		this.serviceIds = extractUsefulServiceIds(args, this);
+		super.start(args);
+		System.out.printf("Generating Squamish Transit System bus data... DONE in %s.\n", Utils.getPrettyDuration(System.currentTimeMillis() - start));
+	}
+
+	@Override
+	public boolean excludeCalendar(GCalendar gCalendar) {
+		if (this.serviceIds != null) {
+			return excludeUselessCalendar(gCalendar, this.serviceIds);
+		}
+		return super.excludeCalendar(gCalendar);
+	}
+
+	@Override
+	public boolean excludeCalendarDate(GCalendarDate gCalendarDates) {
+		if (this.serviceIds != null) {
+			return excludeUselessCalendarDate(gCalendarDates, this.serviceIds);
+		}
+		return super.excludeCalendarDate(gCalendarDates);
+	}
+
+	private static final String INCLUDE_AGENCY_ID = "4"; // Squamish Transit System only
+
+	@Override
+	public boolean excludeRoute(GRoute gRoute) {
+		if (!INCLUDE_AGENCY_ID.equals(gRoute.agency_id)) {
+			return true;
+		}
+		return super.excludeRoute(gRoute);
+	}
+
+	@Override
+	public boolean excludeTrip(GTrip gTrip) {
+		if (this.serviceIds != null) {
+			return excludeUselessTrip(gTrip, this.serviceIds);
+		}
+		return super.excludeTrip(gTrip);
+	}
+
+	@Override
+	public Integer getAgencyRouteType() {
+		return MAgency.ROUTE_TYPE_BUS;
+	}
+
+	@Override
+	public long getRouteId(GRoute gRoute) {
+		return Long.parseLong(gRoute.route_short_name); // use route short name as route ID
+	}
+
+	@Override
+	public String getRouteLongName(GRoute gRoute) {
+		String routeLongName = gRoute.route_long_name;
+		routeLongName = MSpec.CLEAN_SLASHES.matcher(routeLongName).replaceAll(MSpec.CLEAN_SLASHES_REPLACEMENT);
+		routeLongName = MSpec.cleanNumbers(routeLongName);
+		routeLongName = MSpec.cleanStreetTypes(routeLongName);
+		return MSpec.cleanLabel(routeLongName);
+	}
+
+	private static final String AGENCY_COLOR_GREEN = "34B233";// GREEN (from PDF Corporate Graphic Standards)
+	private static final String AGENCY_COLOR_BLUE = "002C77"; // BLUE (from PDF Corporate Graphic Standards)
+
+	private static final String AGENCY_COLOR = AGENCY_COLOR_GREEN;
+
+	@Override
+	public String getAgencyColor() {
+		return AGENCY_COLOR;
+	}
+
+	private static final String COLOR_F78B1F = "F78B1F";
+	private static final String COLOR_004B8D = "004B8D";
+	private static final String COLOR_8CC63F = "8CC63F";
+	private static final String COLOR_FB298C = "FB298C";
+
+	@Override
+	public String getRouteColor(GRoute gRoute) {
+		if (StringUtils.isEmpty(gRoute.route_color)) {
+			int rsn = Integer.parseInt(gRoute.route_short_name);
+			switch (rsn) {
+			// @formatter:off
+			case 1: return COLOR_004B8D;
+			case 2: return COLOR_8CC63F;
+			case 3: return COLOR_F78B1F;
+			case 4: return COLOR_FB298C;
+			// @formatter:on
+			default:
+				return AGENCY_COLOR_BLUE;
+			}
+		}
+		return super.getRouteColor(gRoute);
+	}
+
+	@Override
+	public void setTripHeadsign(MRoute mRoute, MTrip mTrip, GTrip gTrip, GSpec gtfs) {
+		if (mRoute.id == 3l) {
+			if (gTrip.direction_id == 0) {
+				mTrip.setHeadsignInbound(MInboundType.INBOUND);
+				return;
+			} else if (gTrip.direction_id == 1) {
+				mTrip.setHeadsignInbound(MInboundType.OUTBOUND);
+				return;
+			}
+		}
+		mTrip.setHeadsignString(cleanTripHeadsign(gTrip.trip_headsign), gTrip.direction_id);
+	}
+
+	private static final Pattern EXCHANGE = Pattern.compile("(exchange)", Pattern.CASE_INSENSITIVE);
+	private static final String EXCHANGE_REPLACEMENT = "Ex";
+
+	private static final Pattern ENDS_WITH_VIA = Pattern.compile("( via .*$)", Pattern.CASE_INSENSITIVE);
+	private static final Pattern STARTS_WITH_TO = Pattern.compile("(^.* to )", Pattern.CASE_INSENSITIVE);
+
+	private static final Pattern AND = Pattern.compile("( and )", Pattern.CASE_INSENSITIVE);
+	private static final String AND_REPLACEMENT = " & ";
+
+	private static final Pattern CLEAN_P1 = Pattern.compile("[\\s]*\\([\\s]*");
+	private static final String CLEAN_P1_REPLACEMENT = " (";
+	private static final Pattern CLEAN_P2 = Pattern.compile("[\\s]*\\)[\\s]*");
+	private static final String CLEAN_P2_REPLACEMENT = ") ";
+
+	@Override
+	public String cleanTripHeadsign(String tripHeadsign) {
+		tripHeadsign = EXCHANGE.matcher(tripHeadsign).replaceAll(EXCHANGE_REPLACEMENT);
+		tripHeadsign = ENDS_WITH_VIA.matcher(tripHeadsign).replaceAll(StringUtils.EMPTY);
+		tripHeadsign = STARTS_WITH_TO.matcher(tripHeadsign).replaceAll(StringUtils.EMPTY);
+		tripHeadsign = AND.matcher(tripHeadsign).replaceAll(AND_REPLACEMENT);
+		tripHeadsign = CLEAN_P1.matcher(tripHeadsign).replaceAll(CLEAN_P1_REPLACEMENT);
+		tripHeadsign = CLEAN_P2.matcher(tripHeadsign).replaceAll(CLEAN_P2_REPLACEMENT);
+		tripHeadsign = MSpec.cleanStreetTypes(tripHeadsign);
+		return MSpec.cleanLabel(tripHeadsign);
+	}
+
+	private static final Pattern STARTS_WITH_BOUND = Pattern.compile("(^(east|west|north|south)bound)", Pattern.CASE_INSENSITIVE);
+
+	private static final Pattern AT = Pattern.compile("( at )", Pattern.CASE_INSENSITIVE);
+	private static final String AT_REPLACEMENT = " / ";
+
+	@Override
+	public String cleanStopName(String gStopName) {
+		gStopName = STARTS_WITH_BOUND.matcher(gStopName).replaceAll(StringUtils.EMPTY);
+		gStopName = AT.matcher(gStopName).replaceAll(AT_REPLACEMENT);
+		gStopName = EXCHANGE.matcher(gStopName).replaceAll(EXCHANGE_REPLACEMENT);
+		gStopName = MSpec.cleanStreetTypes(gStopName);
+		return MSpec.cleanLabel(gStopName);
+	}
+}
