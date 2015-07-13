04da751cadf15314e82a347c9abb318d807eb9e3
Mathieu Méa <mathieu.mea@gmail.com>
2015-07-13 17:30:19 -0400

    Minor improvements...

--- src/org/mtransit/parser/ca_squamish_transit_system_bus/SquamishTransitSystemBusAgencyTools.java
+++ src/org/mtransit/parser/ca_squamish_transit_system_bus/SquamishTransitSystemBusAgencyTools.java
@@ -36,7 +36,7 @@ public class SquamishTransitSystemBusAgencyTools extends DefaultAgencyTools {
 
 	@Override
 	public void start(String[] args) {
-		System.out.printf("\nGenerating Squamish Transit System bus data...\n");
+		System.out.printf("\nGenerating Squamish Transit System bus data...");
 		long start = System.currentTimeMillis();
 		this.serviceIds = extractUsefulServiceIds(args, this);
 		super.start(args);
