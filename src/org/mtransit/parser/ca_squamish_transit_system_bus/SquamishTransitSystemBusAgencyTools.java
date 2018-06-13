886a3a2f53c4c3fc89c16e8f0027f67a1cb84804
Mathieu Méa <mathieu.mea@gmail.com>
2018-06-13 07:27:24 -0400

    Compatibility with latest update.

--- src/org/mtransit/parser/ca_squamish_transit_system_bus/SquamishTransitSystemBusAgencyTools.java
+++ src/org/mtransit/parser/ca_squamish_transit_system_bus/SquamishTransitSystemBusAgencyTools.java
@@ -194,11 +194,12 @@ public class SquamishTransitSystemBusAgencyTools extends DefaultAgencyTools {
 				.addTripSort(0, //
 						Arrays.asList(new String[] { //
 						"102762", // Northbound Spruce at Chestnut
-								"102729", // Westbound Pemberton at Third
+								"102749", // ++
+								"102729", // Westbound Pemberton at Third #DOWNTOWN
 						})) //
 				.addTripSort(1, //
 						Arrays.asList(new String[] { //
-						"102729", // Westbound Pemberton at Third
+						"102729", // Westbound Pemberton at Third #DOWNTOWN
 								"102734", // ==
 								"102706", // !=
 								"102705", // !=
@@ -226,7 +227,7 @@ public class SquamishTransitSystemBusAgencyTools extends DefaultAgencyTools {
 	@Override
 	public int compareEarly(long routeId, List<MTripStop> list1, List<MTripStop> list2, MTripStop ts1, MTripStop ts2, GStop ts1GStop, GStop ts2GStop) {
 		if (ALL_ROUTE_TRIPS2.containsKey(routeId)) {
-			return ALL_ROUTE_TRIPS2.get(routeId).compare(routeId, list1, list2, ts1, ts2, ts1GStop, ts2GStop);
+			return ALL_ROUTE_TRIPS2.get(routeId).compare(routeId, list1, list2, ts1, ts2, ts1GStop, ts2GStop, this);
 		}
 		return super.compareEarly(routeId, list1, list2, ts1, ts2, ts1GStop, ts2GStop);
 	}
@@ -242,7 +243,7 @@ public class SquamishTransitSystemBusAgencyTools extends DefaultAgencyTools {
 	@Override
 	public Pair<Long[], Integer[]> splitTripStop(MRoute mRoute, GTrip gTrip, GTripStop gTripStop, ArrayList<MTrip> splitTrips, GSpec routeGTFS) {
 		if (ALL_ROUTE_TRIPS2.containsKey(mRoute.getId())) {
-			return SplitUtils.splitTripStop(mRoute, gTrip, gTripStop, routeGTFS, ALL_ROUTE_TRIPS2.get(mRoute.getId()));
+			return SplitUtils.splitTripStop(mRoute, gTrip, gTripStop, routeGTFS, ALL_ROUTE_TRIPS2.get(mRoute.getId()), this);
 		}
 		return super.splitTripStop(mRoute, gTrip, gTripStop, splitTrips, routeGTFS);
 	}
@@ -257,6 +258,16 @@ public class SquamishTransitSystemBusAgencyTools extends DefaultAgencyTools {
 
 	@Override
 	public boolean mergeHeadsign(MTrip mTrip, MTrip mTripToMerge) {
+		List<String> headsignsValues = Arrays.asList(mTrip.getHeadsignValue(), mTripToMerge.getHeadsignValue());
+		if (mTrip.getRouteId() == 1L) {
+			if (Arrays.asList( //
+					"Garibaldi Vlg", //
+					"Downtown" //
+			).containsAll(headsignsValues)) {
+				mTrip.setHeadsignString("Downtown", mTrip.getHeadsignId());
+				return true;
+			}
+		}
 		System.out.printf("\nUnexpected trips to merge: %s & %s!\n", mTrip, mTripToMerge);
 		System.exit(-1);
 		return false;
